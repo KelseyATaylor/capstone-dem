fedb58c49155cd10ad97bb317790f1b8f832c8cb
Kelsey Taylor <kelseytay94@gmail.com>
2022-09-26 16:31:59 -0600

    added like all the things except css

--- server/index.js
+++ server/index.js
@@ -4,9 +4,14 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-const { getDrinks } = require("./controller");
+const { getDrinks, addDrink, deleteDrink, updateLikes } = require("./controller");
 
 app.get("/getDrinks", getDrinks);
+app.post("/addDrink", addDrink);
+app.delete("/deleteDrink/:id", deleteDrink);
+app.put("/updateLikes/:id", updateLikes);
+//you don't need the : when you have it on the front end, : lets the serever know that it should be expecting a parameter
 
 app.listen(4567, () => console.log("Avengers assembled on port 4567!"));
